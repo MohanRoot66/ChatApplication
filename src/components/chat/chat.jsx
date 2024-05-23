c50413312bb3f759cd4219e5268ff7f31eda1a33
Mohan <mohan.amrutham@techwave.net>
2024-05-23 11:57:38 +0530

    Landing screen completed

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "./chat.css"
 import EmojiPicker from 'emoji-picker-react'
 
@@ -13,6 +13,12 @@ export default function Chat() {
       setText((prev)=>prev+e.emoji)
       setIsvisible(false)
   } 
+
+  const endRef = useRef(null);
+
+  useEffect(()=>{
+    endRef.current?.scrollIntoView({behavior: "smooth"})
+  },[])
 
   return (
     <div className='chat'>
@@ -30,7 +36,68 @@ export default function Chat() {
           <img src="./info.png" alt="" />
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur, 
+              adipisicing elit. Adipisci quas quis voluptatibus autem 
+              accusantium amet itaque unde facere? Explicabo, 
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img src="./avatar.png" alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur, 
+              adipisicing elit. Adipisci quas quis voluptatibus autem 
+              accusantium amet itaque unde facere? Explicabo, 
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur, 
+              adipisicing elit. Adipisci quas quis voluptatibus autem 
+              accusantium amet itaque unde facere? Explicabo, 
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur, 
+              adipisicing elit. Adipisci quas quis voluptatibus autem 
+              accusantium amet itaque unde facere? Explicabo, 
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur, 
+              adipisicing elit. Adipisci quas quis voluptatibus autem 
+              accusantium amet itaque unde facere? Explicabo, 
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur, 
+              adipisicing elit. Adipisci quas quis voluptatibus autem 
+              accusantium amet itaque unde facere? Explicabo, 
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div ref={endRef}></div>
+      </div>
       <div className="bottom">
         <div className="icons">
           <img src="./img.png" alt="" />
--- src/components/detail/detail.jsx
+++ src/components/detail/detail.jsx
@@ -3,6 +3,63 @@ import "./detail.css"
 
 export default function Detail() {
   return (
-    <div className='detail'>Details</div>
+    <div className='detail'>
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Jane Doe</h2>
+        <p>Yoo this is amazing</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhSBaZovY9gct87VlCF7TuETnLe3sKswj6ol2vAst4JA&s" alt="" />
+                <span>phototdsf_fsfd.png</span>
+              </div>
+              <img src="./download.png" alt="" className='icon'/>
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhSBaZovY9gct87VlCF7TuETnLe3sKswj6ol2vAst4JA&s" alt="" />
+                <span>phototdsf_fsfd.png</span>
+              </div>
+              <img src="./download.png" alt="" className='icon'/>
+            </div> 
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhSBaZovY9gct87VlCF7TuETnLe3sKswj6ol2vAst4JA&s" alt="" />
+                <span>phototdsf_fsfd.png</span>
+              </div>
+              <img src="./download.png" alt="" className='icon'/>
+            </div>  
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+        <button className='logout'>Logout</button>
+      </div>
+    </div>
   )
 }
